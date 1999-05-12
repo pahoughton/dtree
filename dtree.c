77cfbbb6a0e6df77913fdd8f7c84db3d7c785cb8
houghton <>
1999-05-12 20:13:42 +0000

    Rework args (use getopt). Added 'ignore' support (ignore files named ... (CVS)). More cleanup.

--- dtree.c
+++ dtree.c
@@ -1,150 +1,203 @@
 /*********************************************************************
-*
-*	Title:			dtree
-*
-*	Description:
-*
-*		list directroy tree
-*
-*	Input Agrs:
-*
-*		NONE
-*
-*	Return Values:
-*
-*		NONE
-*
-*	DB access:
-*
-*		procs:
-*
-*		tables:		rw:
-*
-*	Notes:
-*
-*	Programmer:		Paul Houghton
-*
-*	Start Date:		12-04-1991
-*
-*	Modification History:
-*
-*		$Log$
-*		Revision 1.3  1999/05/12 18:59:14  houghton
-*		Cleanup.
-*		
- * Revision 1.1  1991/12/19  01:31:56  paulh
- * Initial revision
  *
-*
-*********************************************************************/
+ * File:        dtree.c
+ * Project:	dtree
+ * Desc:
+ *
+ *  list directroy tree
+ *
+ * Notes:
+ *
+ * Author:	Paul A. Houghton - (paul.houghton@wcom.com)
+ * Created:	12/04/1991
+ *
+ * Revision History: (See end of file for Revision Log)
+ *
+ *  Last Mod By:    $Author$
+ *  Last Mod:	    $Date$
+ *  Version:	    $Revision$
+ *
+ *********************************************************************/
 
-static char RcsId[] = {"$Id$"};
+
+const char dtree_RcsId[] = {
+  "$Id$"
+};
+
+#include "dtree.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <time.h>
 #include <memory.h>
+#include <getopt.h>
 
 #include <log_err.h>
 
 char	usage_msg[] = 
-	{"Usage: %s dirname [-[D|d][S|s][F|f]] [-l logfilename]\n"};
-	
+{
+  "Usage: %s dirname [-[D|d][S|s][F|f]] [-l logfilename]\n"
+  "\n"
+  "  -D         turn on debug output\n"
+  "  -d         turn off debug output (default)\n"
+  "  -S	        trun on log to screen (default)\n"
+  "  -s         turn off log to screen\n"
+  "  -F         turn on log to file\n"
+  "  -s         turn of log to file\n"
+  "  -l fn      log file name\n"
+  "  -i ignore  file names to ignore (CVS|RCS|SCCS)\n"
+};
+
+const char ** Ignore = 0;
+
+static const char * IgnoreSep = { " \t,|:" };
+
 int main(int argc, char *argv[])
 {
 
-	int				optind;				/* program arg */
-	int				flags;
-	int				opt;				/* program option */
-	int				req_arg_cnt = 0;	/* required arg count */	
-	char			log_fn[FILENAME_MAX];
+  int	opt;			/* program option */
+  char	log_fn[FILENAME_MAX];
+
+  char *    ignoreArg = strdup( "CVS|RCS|SCCS" );
+
+  /* Default Log Options */
+  log_debug(OFF);
+  log_screen(ON);
+  log_file(OFF);
+
+  sprintf(log_fn,"%s.%s","error",argv[0]);
+  log_filename(log_fn);
+    
+  /* process program options and bp_begin/end */
 	
-	char			dir_name[FILENAME_MAX];
-	
+  if(argc < 1)
+    {
+      fprintf(stderr,usage_msg,argv[0],"Too Few Args");
+      exit(1);
+    }
 
-strcpy(dir_name,".");	
+  /* get begin bill period date */
 
-	/* Default Log Options */
-log_debug(OFF);
-log_screen(ON);
-log_file(OFF);
-
-sprintf(log_fn,"%s.%s","error",argv[0]);
-log_filename(log_fn);
-
-	/* process program options and bp_begin/end */
-	
-if(argc < 1)
+  while( (opt = getopt( argc, argv, "DdFfi:Ssl:" )) != EOF )
+    {
+      switch( opt )
 	{
-	fprintf(stderr,usage_msg,argv[0],"Too Few Args");
-	exit(1);
+	case 'D':
+	  log_debug( ON );
+	  break;
+	  
+	case 'd':
+	  log_debug(OFF);
+	  break;
+	  
+	case 'F':
+	  log_file(ON);
+	  break;
+	  
+	case 'f':
+	  log_file(OFF);
+	  break;
+	  
+	case 'i':
+	  ignoreArg = strdup( optarg );
+	  break;
+	  
+	case 'S':
+	  log_screen(ON);
+	  break;
+	  
+	case 's':
+	  log_screen(OFF);
+	  break;
+	  
+	case 'l':
+	  optind++;
+	  log_filename( optarg );
+	  break;
+	  
+	default:
+	  fprintf(stderr, usage_msg, argv[0] );
+	  exit(1);
+	}
+    }
+
+  if( ignoreArg )
+    {
+      size_t  ig = 0;
+      size_t  isize = 16;
+      char *  tokarg = ignoreArg;
+      char *  i;
+      
+      if( (Ignore = (const char **)malloc( sizeof( char * ) * isize ))
+	  == NULL )
+	{
+	  log_err(errno,0,1,"malloc failed." );
+	  exit( 1 );
 	}
 
-	/* get begin bill period date */
-
-for(optind = 1; optind < argc; optind++)
+      Ignore[ ig ] = strdup( "." );
+      ++ ig;
+      Ignore[ ig ] = strdup( ".." );
+      ++ ig;
+      Ignore[ ig ] = 0;
+      
+      while( ( i = strtok( tokarg, IgnoreSep )) != NULL )
 	{
-	if(argv[optind][0] == '-')
+	  tokarg = 0;
+	  if( ig + 2 > isize )
+	    {
+	      isize += 16;
+	      if( (Ignore = (const char **)realloc( Ignore,
+						    sizeof(char *) * isize ) )
+		  == NULL )
 		{
-		flags = optind;
-		for(opt = 1;argv[flags][opt] != 0; opt++)
-			{
-			switch(argv[optind][opt])	
-				{
-				case 'D':
-					log_debug(ON);
-					break;
-				
-				case 'd':
-					log_debug(OFF);
-					break;
-				
-				case 'F':
-					log_file(ON);
-					break;
-				
-				case 'f':
-					log_file(OFF);
-					break;
-					
-				case 'S':
-					log_screen(ON);
-					break;
-				
-				case 's':
-					log_screen(OFF);
-					break;
-					
-				case 'l':
-					optind++;
-					log_filename(argv[optind]);
-					break;
-									
-					
-				default:
-					fprintf(stderr,usage_msg,argv[0],argv[optind]);
-					exit(1);
-				}
-			}
+		  log_err(errno,0,1,"realloc failed.");
+		  exit( 1 );
 		}
-	else
-		{
-		switch(req_arg_cnt)
-			{
-			case 0:
-				strcpy(dir_name,argv[optind]);
-				req_arg_cnt++;
-				break;
-			
-			default:
-				fprintf(stderr,usage_msg,argv[0],argv[optind]);
-				exit(1);
-			
-			}
-		}
+	    }
+	  
+	  Ignore[ ig ] = strdup( i );
+	  ++ ig;
+	  Ignore[ ig ] = 0;
 	}
+    }
 
-dir_tree(dir_name,0);
-exit(0);
+  {
+    const char ** i;
+    
+    for( i = Ignore; *i; ++ i )
+      {
+	log_err( 0, 0, DEBUG_MSG, "will ignore '%s'", *i );
+      }
+  }
+  
+  if( optind >= argc )
+    {
+      dir_tree( ".", 0 );
+    }
+  else
+    {
+      for( ; optind < argc; ++ optind )
+	dir_tree( argv[ optind ] ,0 );
+    }
+  
+  return(0);
 }
+/*
+ *
+ * Revision Log:
+ *
+ * $Log$
+ * Revision 1.4  1999/05/12 20:13:42  houghton
+ * Rework args (use getopt).
+ * Added 'ignore' support (ignore files named ... (CVS)).
+ * More cleanup.
+ *
+ * Revision 1.1  1991/12/19  01:31:56  paulh
+ * Initial revision
+ *
+ * Revision 1.3  1999/05/12 18:59:14  houghton
+ * Cleanup.
+ */
+
