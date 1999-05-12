1fbf11cc6b051c06c6b3a6147f35a370b5d8e1b5
houghton <>
1999-05-12 20:13:04 +0000

    Initial Version.

---
+++ dtree.h
@@ -0,0 +1,40 @@
+#ifndef _dtree_h_
+#define _dtree_h_
+/*********************************************************************
+ *
+ * Title:       dtree.h
+ * Project:	dtree
+ * Desc:
+ *
+ *  
+ *
+ * Notes:
+ *
+ * Author:	Paul A. Houghton - (paul.houghton@wcom.com)
+ * Created:	05/12/99 13:15
+ *
+ * Revision History: (See end of file for Revision Log)
+ *
+ *  Last Mod By:    $Author$
+ *  Last Mod:	    $Date$
+ *  Version:	    $Revision$
+ *
+ * $Id$
+ *
+ *********************************************************************/
+
+int dir_tree( const char * dir_name, int level);
+
+
+/*
+ *
+ * Revision Log:
+ *
+ * $Log$
+ * Revision 1.1  1999/05/12 20:13:04  houghton
+ * Initial Version.
+ *
+ *
+ */
+
+#endif /* ! def _dtree_h_ */
